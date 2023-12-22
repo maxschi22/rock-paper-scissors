7c89b857e90f610a1c2d12618f1718c80015e063
Max Schimmel <mschimmel@hansalog.de>
2023-12-22 11:46:02 +0100

    Added Function getComputerChoice and Function getPlayerChoice

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 console.log("Rock Paper Scissors Game");
 
-function getComputerChoice(computerChoice) {
-    const rock = 1;
-    const paper = 2;
-    const scissors = 3;
-    return Math.random()
+function getComputerChoice() {
+    let choiceRandomized = Math.floor(Math.random() * 3);
+    if (choiceRandomized == 1) {
+        console.log ("rock");
+    } else if (choiceRandomized == 2) {
+        console.log ("paper");
+    } else {
+        console.log ("scisscors");
+    }
+}
+
+function getPlayerChoice() {
+    const usersChoice = prompt("Rock Paper or Scissors?");
+    console.log(usersChoice);
+    return usersChoice;
 }
 
 function playRound (playerChoice, computerChoice) {
-    
+
 }
+
+getComputerChoice();
+getPlayerChoice();
