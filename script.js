2e35e9c5fb38727a6fc22cea6a7afc349fdaccc3
Max Schimmel <mschimmel@hansalog.de>
2023-12-22 12:04:17 +0100

    added return values in getComputerChoice and added if to getPlayerChoice to prevent false inputs

--- script.js
+++ script.js
@@ -4,21 +4,28 @@ function getComputerChoice() {
     let choiceRandomized = Math.floor(Math.random() * 3);
     if (choiceRandomized == 1) {
         console.log ("rock");
+        return "rock";
     } else if (choiceRandomized == 2) {
         console.log ("paper");
+        return "paper";
     } else {
-        console.log ("scisscors");
+        console.log ("scissors");
+        return "scissors";
     }
 }
 
 function getPlayerChoice() {
     const userChoice = prompt("Rock Paper or Scissors?");
+    userChoice.toLowerCase();
     console.log(userChoice);
+    if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
+        alert("Only use Rock, Paper or Scissors")
+    }
     return userChoice.toLowerCase();
 }
 
 function playRound (playerChoice, computerChoice) {
-    if (playerChoice == computerChoice) {
+    if (playerChoice === computerChoice) {
         console.log("draw");
         return "draw";
     } else if (playerChoice == "paper" && computerChoice == "rock") {
