74f8085b3e6ef20592d6522687bab5d2dcdf98b0
Max Schimmel <mschimmel@hansalog.de>
2023-12-22 11:21:17 +0100

    Added index.html and script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Rock Paper Scissors Game");
+
+function getComputerChoice(computerChoice) {
+    const rock = 1;
+    const paper = 2;
+    const scissors = 3;
+    return Math.random()
+}
+
+function playRound (playerChoice, computerChoice) {
+    
+}
